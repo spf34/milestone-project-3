d9564d383e9a64c62a171db62f03631f87ee562c
spf34 <samuel.forster@cantab.net>
2021-04-30 16:43:56 +0000

    Add DB connection and historical data load files

---
+++ fetch_yahoo_data.py
@@ -0,0 +1,17 @@
+import seaborn as sns
+import yfinance as yf
+import datetime as dtm
+import matplotlib.pyplot as plt
+
+sns.set(rc={'figure.figsize': (18, 6)})
+
+TICKERS = ('AGG', 'EMB', 'HYG', 'IAU', 'IEMG', 'IEFA', 'MBB', 'QQQ', 'SDY', 'SPY',
+           'TIP', 'VTV', 'VUG', 'XLC', 'XLE', 'XLF', 'XLI', 'XLK', 'XLV', 'XLY')
+
+today = dtm.date.today()
+end = dtm.datetime.strftime(today, '%Y-%m-%d')
+data = yf.download(TICKERS, start='2016-01-01', end=end)['Adj Close']
+
+data *= 100 / data.iloc[0]
+data.plot();
+plt.legend(loc=2, fontsize='x-small')
