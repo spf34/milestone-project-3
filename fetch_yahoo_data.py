9f7b1d3418a4d79387d904fb2a68cc9eaef5a666
spf34 <samuel.forster@cantab.net>
2021-04-30 21:55:48 +0000

    Refactor fetch_yahoo_data.py

--- fetch_yahoo_data.py
+++ fetch_yahoo_data.py
@@ -5,13 +5,19 @@ import matplotlib.pyplot as plt
 
 sns.set(rc={'figure.figsize': (18, 6)})
 
-TICKERS = ('AGG', 'EMB', 'HYG', 'IAU', 'IEMG', 'IEFA', 'MBB', 'QQQ', 'SDY', 'SPY',
-           'TIP', 'VTV', 'VUG', 'XLC', 'XLE', 'XLF', 'XLI', 'XLK', 'XLV', 'XLY')
+TICKERS = (
+    'AGG', 'EMB', 'HYG', 'IAU', 'IEMG', 'IEFA', 'MBB',
+    'QQQ', 'SDY', 'SPY', 'TIP', 'VTV', 'VUG', 'XLC',
+    'XLE', 'XLF', 'XLI', 'XLK', 'XLV', 'XLY'
+    )
 
 today = dtm.date.today()
 end = dtm.datetime.strftime(today, '%Y-%m-%d')
-data = yf.download(TICKERS, start='2016-01-01', end=end)['Adj Close']
+data = yf.download(
+    TICKERS,
+    start='2016-01-01',
+    end=end,
+    progress=False
+    )['Adj Close']
 
 data *= 100 / data.iloc[0]
-data.plot();
-plt.legend(loc=2, fontsize='x-small')
