98f9e7400ad6af8e88a8da6368a157e8d85f4e27
spf34 <samuel.forster@cantab.net>
2021-05-04 09:18:57 +0000

    Fix to ensure portfolio displayed in same column order

--- app.py
+++ app.py
@@ -158,6 +158,7 @@ def portfolio_overview(username):
         return redirect(url_for('login'))
 
     portfolio = pd.DataFrame(get_portfolio_from_username(username))
+    portfolio = portfolio[list(TICKERS)]
     
     if len(portfolio):
         portfolio = portfolio.reset_index('date')
